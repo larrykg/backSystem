99916383a3d40933c6d38237432fbabfa0cefe21
larry <492178939@qq.com>
2022-02-23 14:26:07 +0800

    修改|更新品牌：静态结构

--- src/views/product/tradeMark/TradeMark.vue
+++ src/views/product/tradeMark/TradeMark.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <el-button type="primary" icon="el-icon-plus" style="margin: 10px 0px">添加</el-button>
+    <el-button type="primary"
+               icon="el-icon-plus"
+               style="margin: 10px 0px"
+               @click="showDailog"
+    >添加
+    </el-button>
     <!--    data:数据 border边框  label 标题-- width 宽度> -->
     <el-table style="width: 100%" border :data="list">
       <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
@@ -12,7 +17,7 @@
       </el-table-column>
       <el-table-column prop="prop" label="操作" width="width">
         <template slot-scope="{row,$index}">
-          <el-button type="warning" icon="el-icon-edit" size="limit">修改</el-button>
+          <el-button type="warning" icon="el-icon-edit" size="limit" @click="updateTradeMark">修改</el-button>
           <el-button type="danger" icon="el-icon-delete" size="limit">删除</el-button>
         </template>
       </el-table-column>
@@ -28,8 +33,32 @@
       @size-change='handleSizeChange'
       layout="prev,pager,next,jumper,->,sizes,total"
     >
-
     </el-pagination>
+
+    <el-dialog title="添加品牌" :visible.sync="dialogFormVisible" style="width: 80%">
+      <el-form>
+        <el-form-item label="品牌名称" label-width="100px">
+          <el-input autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" label-width="100px">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -41,7 +70,10 @@
         page: 1,
         limit: 3,
         total: 0,
-        list: []
+        list: [],
+        dialogFormVisible: false,
+        //上传图片
+        imageUrl: ''
       }
     },
     mounted() {
@@ -60,9 +92,32 @@
         }
       },
       //展示数据的条数发生变化的时候触发
-      handleSizeChange(limit){
+      handleSizeChange(limit) {
         this.limit = limit;
         this.getPageList()
+      },
+      //显示对话框
+      showDailog() {
+        this.dialogFormVisible = true
+      },
+      //修改
+      updateTradeMark() {
+        this.dialogFormVisible = true
+      },
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
       }
     }
 
@@ -70,5 +125,30 @@
 </script>
 
 <style scoped>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
 
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
