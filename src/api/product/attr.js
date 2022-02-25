9ee28ae2e6cbe76b0e7a9433586430e3860fbeda
larry <492178939@qq.com>
2022-02-25 14:46:06 +0800

    平台属性管理：属性值添加修改的事件

--- src/api/product/attr.js
+++ src/api/product/attr.js
@@ -21,7 +21,7 @@ export const reqCategory2List = (category1Id) => {
 //获取三级分类数据 /admin/product/getCategory3/{category2Id}
 export const reqCategory3List = (category2Id) => {
   return request({
-    url: `/admin/product/getCategory2/${category2Id}`,
+    url: `/admin/product/getCategory3/${category2Id}`,
     method: 'get'
   })
 }
@@ -34,3 +34,24 @@ export const reqAttrList = (category1Id, category2Id, category3Id) => {
     }
   )
 }
+//添加属性与属性值  POST /admin/product/saveAttrInfo
+export const reqAddAttr = (data) => {
+  return request({
+    url: '/admin/product/saveAttrInfo',
+    method: 'post',
+    data
+  })
+}
+// {
+//   "attrName": "string",
+//   "attrValueList": [
+//   {
+//     "attrId": 0,
+//     "id": 0,
+//     "valueName": "string"
+//   }
+// ],
+//   "categoryId": 0, category3Id
+//   "categoryLevel": 0, 3
+//   "id": 0
+// }
--- src/views/product/attr/Attr.vue
+++ src/views/product/attr/Attr.vue
@@ -5,7 +5,7 @@
     </el-card>
     <el-card>
       <div v-show="isTableShow">
-        <el-button type="primary" icon="el-icon-plus" :disabled="!category3Id" @click="isTableShow=false">添加属性
+        <el-button type="primary" icon="el-icon-plus" :disabled="!category3Id" @click="addAttr">添加属性
         </el-button>
         <!--      表格数据-->
         <el-table border="" style="width:100%" :data="attrList">
@@ -20,7 +20,7 @@
           </el-table-column>
           <el-table-column prop="prop" label="操作" width="150px">
             <template slot-scope="{row,$index}">
-              <el-button type="warning" icon="el-icon-edit" size="min" @click="isTableShow=false"></el-button>
+              <el-button type="warning" icon="el-icon-edit" size="min" @click="updateAttr(row)"></el-button>
               <el-button type="danger" icon="el-icon-delete"></el-button>
             </template>
           </el-table-column>
@@ -29,22 +29,38 @@
 
       <!--      添加|修改属性-->
       <div v-show="!isTableShow">
-        <el-form :inline="true" ref="form" label-width="80px">
+        <el-form :inline="true" ref="form" label-width="80px" :model="attrInfo">
           <el-form-item label="属性名">
-            <el-input placeholder="请输入属性名">
+            <el-input placeholder="请输入属性值" v-model="attrInfo.attrName">
             </el-input>
           </el-form-item>
         </el-form>
-        <el-button type="primary" icon="el-icon-plus">添加属性值</el-button>
+        <el-button type="primary" icon="el-icon-plus" @click="addAttrValue" :disabled="!attrInfo.attrName">添加属性值
+        </el-button>
         <el-button @click="isTableShow=true">取消</el-button>
 
-        <el-table border style="width:100%;margin: 20px 0">
+        <el-table border style="width:100%;margin: 20px 0" :data="attrInfo.attrValueList">
 
           <el-table-column type="index" label="序号" width="80" align="center"></el-table-column>
 
-          <el-table-column label="属性值名称" width="width" prop="prop"></el-table-column>
+          <el-table-column label="属性值名称" width="width" prop="prop">
+            <template slot-scope="{row,$index}">
+              <el-input v-model="row.valueName"
+                        placeholder="请输入属性值名称"
+                        size="mini" v-if="row.flag"
+                        @blur="toLook(row)"
+                        @keyup.native.enter="toLook(row)"
+                        :ref="$index"
+              ></el-input>
+              <span v-else @click="toEdit(row,$index)" style="display: block">{{row.valueName}}</span>
+            </template>
+          </el-table-column>
 
-          <el-table-column label="操作" width="width" prop="prop"></el-table-column>
+          <el-table-column label="操作" width="width" prop="prop">
+            <template slot-scope="{row,$index}">
+              <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            </template>
+          </el-table-column>
 
         </el-table>
         <el-button type="primary">保存</el-button>
@@ -55,6 +71,9 @@
 </template>
 
 <script>
+  //引入lodash当中的深拷贝
+  import cloneDeep from 'lodash/cloneDeep';
+
   export default {
     name: "Attr",
     data() {
@@ -64,7 +83,14 @@
         category3Id: '',
         attrList: [],
         //属性值列表的展示
-        isTableShow: false
+        isTableShow: true,
+        //收集新增属性|修改属性
+        attrInfo: {
+          "attrName": "",
+          "attrValueList": [],
+          "categoryId": 0,
+          "categoryLevel": 3
+        }
       }
     },
     methods: {
@@ -89,6 +115,63 @@
         if (result.code == 200) {
           this.attrList = result.data
         }
+      },
+      //添加属性值
+      addAttrValue() {
+        this.attrInfo.attrValueList.push({
+          attrId: this.attrInfo.id,
+          valueName: "",
+          //切换编辑模式|查看模式
+          flag: true
+        });
+        this.$nextTick(() => {
+          this.$refs[this.attrInfo.attrValueList.length - 1].focus()
+        })
+      },
+      //添加属性按钮的回调
+      addAttr() {
+        //切换table的隐藏
+        this.isTableShow = false;
+        //清除已写入的model数据
+        this.attrInfo = {
+          attrName: '',
+          attrValueList: [],
+          categoryId: this.category3Id,
+          categoryLevel: 3
+        }
+      },
+      //修改某一个属性值
+      updateAttr(row) {
+        this.isTableShow = false;
+        //由于数据结构是对象>数组>对象
+        this.attrInfo = cloneDeep(row);
+        //在修改某属性的时候 将相应的元素添加flag标记
+        this.attrInfo.attrValueList.forEach(item => {
+          //VUE 无法探测普通的新增的属性: item.flag = false
+          this.$set(item, 'flag', false)
+        })
+      },
+      //失去焦点事件 切换为查看模式
+      toLook(row) {
+        if (row.valueName.trim() == '') {
+          this.$message('请输入正常属性值');
+          return
+        }
+        let isRepat = this.attrInfo.attrValueList.some(item => {
+          if (row != item) {
+            return row.valueName == item.valueName
+          }
+        });
+        if (isRepat) return;
+        row.flag = false
+      },
+      //点击SPAN的事件
+      toEdit(row, index) {
+        row.flag = true;
+        //获取input节点  实现聚焦  点击span时切换input ，对于浏览器页面重绘需要耗时间，需要$nextTick 节点渲染完毕了执行一次
+        this.$nextTick(() => {
+          this.$refs[index].focus()
+        })
       }
     }
 
